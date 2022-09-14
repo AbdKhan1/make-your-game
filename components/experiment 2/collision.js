b886ace05f52c1a85484c3fd4e345337d570010e
Nik <nishak.don@gmail.com>
2022-09-14 11:58:30 +0100

    try and improve the collision

--- components/experiment 2/collision.js
+++ components/experiment 2/collision.js
@@ -4,9 +4,9 @@ import { removeBrick } from "./bricks.js"
 
 // DOMRect describes the size and position of a rectangle.
 
-export function checkCollision(ball,objectDOMRect) {
+export function checkCollision(ball, objectDOMRect) {
     // https://youtu.be/r0sy-Cr6WHY?t=327
-    
+
     if (ball.x > objectDOMRect.x + objectDOMRect.width ||
         ball.x + ball.width < objectDOMRect.x ||
         ball.y > objectDOMRect.y + objectDOMRect.height ||
@@ -22,7 +22,7 @@ export function checkCollision(ball,objectDOMRect) {
 
 export function checkPaddleCollision(ballDOMRect) {
     let paddleDOMRect = document.querySelector(".paddle").getBoundingClientRect()
-    return checkCollision(ballDOMRect,paddleDOMRect)
+    return checkCollision(ballDOMRect, paddleDOMRect)
 }
 
 export function checkWallCollision(ballDOMRect) {
@@ -30,17 +30,17 @@ export function checkWallCollision(ballDOMRect) {
     // check collision with walls
     // with left wall
     let leftW = document.querySelector(".leftWall")
-    if (checkCollision(ballDOMRect,leftW.getBoundingClientRect())) {
+    if (checkCollision(ballDOMRect, leftW.getBoundingClientRect())) {
         return "left"
     }
     // with right wall
     let rightW = document.querySelector(".rightWall")
-    if (checkCollision(ballDOMRect,rightW.getBoundingClientRect())) {
+    if (checkCollision(ballDOMRect, rightW.getBoundingClientRect())) {
         return "right"
     }
     // with top wall
     let topW = document.querySelector(".topWall")
-    if (checkCollision(ballDOMRect,topW.getBoundingClientRect())) {
+    if (checkCollision(ballDOMRect, topW.getBoundingClientRect())) {
         return "top"
     }
     // with bottom wall
@@ -60,12 +60,12 @@ export function checkBrickCollision(ballDOMRect) {
 
     for (let i = 0; i < bricks.length; i++) {
         let brickDOMRect = bricks[i].getBoundingClientRect()
-        if (checkCollision(ballDOMRect, brickDOMRect)) {     
+        if (checkCollision(ballDOMRect, brickDOMRect)) {
             // removeBrick(i) 
             return i
         }
     }
 
-  
+
 
 }
