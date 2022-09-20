acb7db26233ffb12ccbed9f37d2152f9233bae3f
gymlad <abdalraheemkhan@icloud.com>
2022-09-20 15:12:32 +0100

    make borders responsive

--- components/experiment2/collision.js
+++ components/experiment2/collision.js
@@ -1,8 +1,13 @@
 import { gameViewSettings } from "./globalsettings.js"
+import { frames } from "./script.js"
 
+export function changeColor(DOM, fps) {
+    if (fps % 30 === 0) {
+        DOM.style.backgroundColor = 'gray'
+    }
+}
 
 // DOMRect describes the size and position of a rectangle.
-
 export function checkCollision(ball, objectDOMRect) {
     // https://youtu.be/r0sy-Cr6WHY?t=327
 
@@ -16,7 +21,6 @@ export function checkCollision(ball, objectDOMRect) {
         // console.log("collision!!")
         return true
     }
-
 }
 
 export function checkPaddleCollision(ballDOMRect) {
@@ -29,25 +33,41 @@ export function checkWallCollision(ballDOMRect) {
     // check collision with walls
     // with left wall
     let leftW = document.querySelector(".leftWall")
+    let leftWCorner = document.querySelector('.leftWallCorner')
     if (checkCollision(ballDOMRect, leftW.getBoundingClientRect())) {
+        leftW.style.backgroundColor = 'Silver'
+        leftWCorner.style.backgroundColor = 'Silver'
         return "left"
     }
+    changeColor(leftW, frames)
+    changeColor(leftWCorner, frames)
+
     // with right wall
     let rightW = document.querySelector(".rightWall")
+    let rightWCorner = document.querySelector('.rightWallCorner')
     if (checkCollision(ballDOMRect, rightW.getBoundingClientRect())) {
+        rightW.style.backgroundColor = 'Silver'
+        rightWCorner.style.backgroundColor = 'Silver'
         return "right"
     }
+    changeColor(rightW, frames)
+    changeColor(rightWCorner, frames)
+
     // with top wall
     let topW = document.querySelector(".topWall")
     if (checkCollision(ballDOMRect, topW.getBoundingClientRect())) {
+        topW.style.backgroundColor = 'Silver'
+        rightWCorner.style.backgroundColor = 'Silver'
+        leftWCorner.style.backgroundColor = 'Silver'
         return "top"
     }
+    changeColor(topW, frames)
+
     // with bottom wall
     let paddleDOMRect = document.querySelector(".paddle").getBoundingClientRect()
     if (ballDOMRect.y > (paddleDOMRect.y + (2 * paddleDOMRect.height))) {
         return "bottom"
     }
-
     return "none"
 
 }
